5b3b755435ca8c984472365db45315e8b5fcdba7
Mario Mancilla <mariormt17@gmail.com>
2021-01-04 00:25:41 -0600

    Creando la vista principal y estados iniciales

--- src/model/state.rb
+++ src/model/state.rb
@@ -1,17 +1,29 @@
 # Representación del estado del juego
 
 module Model
-    class Coordinate < Struct.new(row, col)
+    class Coordinate < Struct.new(:row, :col)
+    end
 
     class Food < Coordinate
     end
     
-    class Snake < Struct.new(positions)
+    class Snake < Struct.new(:positions)
     end
 
-    class Grid < Struct.new(rows, cols)
+    class Grid < Struct.new(:rows, :cols)
     end
 
-    class State < Struct.new(snake, food, grid)        
+    class State < Struct.new(:snake, :food, :grid)
+    end
+
+    def self.initial_state
+        Model::State.new(
+            Model::Snake.new([
+                Model::Coordinate.new(1,1),
+                Model::Coordinate.new(1,0)
+            ]),
+            Model::Food.new(4,4),
+            Model::Grid.new(8,12)
+        )
     end
 end
